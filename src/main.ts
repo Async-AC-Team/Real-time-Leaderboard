537aaedcd23ccc308e395bb18bac73d624397134
SORVER <boodymostaf24@gmail.com>
2025-10-23 21:58:14 +0300

    adding a validation pipe to add dtos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    /*
+    Warning for the AC team:
+    When whitelist is true, NestJS will automatically remove any properties from the incoming data that are not defined in your DTO class.
+    When forbidNonWhitelisted is true, instead of removing the extra properties, NestJS will throw an error if it finds any field not defined in your DTO.
+    When transform is true, NestJS will automatically transform payloads to be objects typed according to their DTO classes.
+    
+    # hint for the future: while making users module add a dto folder inside users module and define your dtos there.
+    # TODO: add common dtos for requests and responses
+  */
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+  
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
